9a795e5b333dca1c7c520d758a6a0b2388801250
Mick L. <mickdbf88@gmail.com>
2025-08-17 20:27:45 -0700

    test push->deploy

--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-templates = Jinja2Templates(directory="app/templates")
+# Remove 'app/' prefix
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "Welcome to ProjectX"})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
