a751f73c7a7d5ff057be0e19ec58a0dfbc32ec75
Mick L. <mickdbf88@gmail.com>
2025-08-17 19:42:20 -0700

    test

--- app/main.py
+++ app/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-import os
-import uvicorn
 
 app = FastAPI()
 
@@ -13,7 +11,3 @@ templates = Jinja2Templates(directory="app/templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "Welcome to ProjectX"})
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Use Cloud Run's PORT or fallback to 8080
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
